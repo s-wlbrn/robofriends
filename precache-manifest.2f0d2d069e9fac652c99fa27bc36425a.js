b344f5fe08fb58c3e9794c65aeea17ca72b85a56
swelbourn <welbourn.stephen@gmail.com>
2020-05-07 19:37:58 -0500

    Updates

---
+++ precache-manifest.2f0d2d069e9fac652c99fa27bc36425a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f260c8aad694926a814e3d7d4a47e002",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b7ec8c6b2ea758c0459f",
+    "url": "/robofriends/static/css/main.a6c92cb6.chunk.css"
+  },
+  {
+    "revision": "ceb2409d7ba2dce6be8f",
+    "url": "/robofriends/static/js/2.d1a8ed56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d1a8ed56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ec8c6b2ea758c0459f",
+    "url": "/robofriends/static/js/main.d3e92ef5.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
